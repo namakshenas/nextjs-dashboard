30aa53f1b3b476495d623f462f336ab6995cb005
M. Namakshenas <m.n.shenas@gmail.com>
2025-07-13 13:44:11 +0330

    Add update and delete invoice features

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 ## Next.js Dashboard App
 
 This is the starter template for the Next.js App Router for the dashboard application (with local db).
-
-
--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,8 @@
 'use server';
 
 import { z } from 'zod';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 import postgres from 'postgres';
  
 // const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
@@ -29,7 +31,33 @@ export async function createInvoice(formData: FormData) {
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
-  // Test it out:
-//   console.log(rawFormData);
-//   console.log(typeof rawFormData.amount);
+  
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
 }
